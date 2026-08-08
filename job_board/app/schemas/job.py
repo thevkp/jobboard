2f8bb04ac03a0b94d27cc2541a43e1e0b02e3ce9
thevkp <vishalkumarpatel@gmail.com>
2026-08-08 20:15:18 +0530

    fix job update model and job read model

--- job_board/app/schemas/job.py
+++ job_board/app/schemas/job.py
@@ -4,14 +4,16 @@ from app.schemas.skill import SkillRead
 
 
 class JobCreate(BaseModel):
+    # what a client sends to CREATE a job
     title: str
     description: str
     location: str
     min_salary: int
     max_salary: int
-    company_id: int
+    # company_id: int # this should not 
 
 class JobRead(BaseModel):
+    # what the SERVER sends BACK to the client
     model_config = ConfigDict(from_attributes=True)
 
     id: int
@@ -24,12 +26,13 @@ class JobRead(BaseModel):
     skills: list[SkillRead] = []
 
 class JobUpdate(BaseModel):
+    # what a client sends to UPDATE a job - all optional
     model_config = ConfigDict(from_attributes=True)
 
-    title: str
-    description: str
-    location: str
-    min_salary: int
-    max_salar: int
-    company: CompanyRead
+    title: str | None = None
+    description: str | None = None
+    location: str | None = None
+    min_salary: int | None = None
+    max_salary: int | None = None
+    # company: CompanyRead 
     skills: list[SkillRead] = []
